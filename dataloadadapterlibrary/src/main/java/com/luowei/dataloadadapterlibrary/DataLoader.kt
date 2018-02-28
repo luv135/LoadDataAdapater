d0c7bf7654581bf3679fb4ad08f4286c7af988f3
luowei <luv135@163.com>
2018-02-28 14:44:17 +0800

    clean

--- dataloadadapterlibrary/src/main/java/com/luowei/dataloadadapterlibrary/DataLoader.kt
+++ dataloadadapterlibrary/src/main/java/com/luowei/dataloadadapterlibrary/DataLoader.kt
@@ -4,35 +4,44 @@ import android.os.Handler
 import android.support.v7.widget.RecyclerView
 import android.util.LruCache
 import com.unistrong.luowei.commlib.Log
+import java.lang.ref.WeakReference
 import java.util.*
 
 /**
  * Created by luowei on 2017/12/23.
  *
  */
-class DataLoader<out K, VH : RecyclerView.ViewHolder>(private val adapter: RecyclerView.Adapter<VH>) : Runnable {
+class DataLoader<out K, VH : RecyclerView.ViewHolder>(private val adapter: WeakReference<RecyclerView.Adapter<VH>>) : Runnable {
     override fun run() {
         Log.d("background run is start...")
         while (isRun) {
             if (task.size == 0) synchronized(objecz, { objecz.wait(60000) })
-            val position = task.peek()
-            position ?: break
-            val loadData = (adapter as Loader<K>).loadData(position, position + SECTION)
-            loadData.forEachIndexed { index, s -> itemData.put(index + position, s) }
-
-            Log.d(itemData.snapshot().entries.sortedBy { it.key }.map { "[${it.key},${it.value}]" }.toString())
-            Log.d("firstOrNull=${itemData.snapshot().keys.firstOrNull() ?: 0}," +
-                    "lastOrNull=${itemData.snapshot().keys.lastOrNull() ?: 0}")
-            section[0] = itemData.snapshot().keys.first()
-            section[1] = itemData.snapshot().keys.last()
-            handler.post {
-                Log.d("notify data change")
-                adapter.notifyItemRangeChanged(position, loadData.size)
-            }
-            task.poll()
+            if (work()) break
         }
         Log.d("background run is dead !!")
         isRun = false
+    }
+
+    private fun work(): Boolean {
+        val position = task.peek()
+        position ?: return true
+        Log.d("load [$position, ${position + SECTION}]")
+        val loadData = (adapter.get() as? Loader<K>)?.loadData(position, position + SECTION)
+        Log.d("load finish")
+        loadData ?: return true
+        loadData.forEachIndexed { index, s -> itemData.put(index + position, s) }
+
+        //            Log.d(itemData.snapshot().entries.sortedBy { it.key }.map { "[${it.key},${it.value}]" }.toString())
+        //            Log.d("firstOrNull=${itemData.snapshot().keys.firstOrNull() ?: 0}," +
+        //                    "lastOrNull=${itemData.snapshot().keys.lastOrNull() ?: 0}")
+        section[0] = itemData.snapshot().keys.first()
+        section[1] = itemData.snapshot().keys.last()
+        handler.post {
+            Log.d("notify data change [$position, ${loadData.size}]")
+            adapter.get()?.notifyItemRangeChanged(position, loadData.size)
+        }
+        task.poll()
+        return false
     }
 
     companion object {
@@ -52,18 +61,29 @@ class DataLoader<out K, VH : RecyclerView.ViewHolder>(private val adapter: Recyc
         fun loadData(start: Int, end: Int): Array<K>
     }
 
-    fun getItem(postion: Int): K? {
-        starBackgroundTask()
-        val data = itemData[postion]
-        val down = postion >= lastPosition
+    fun clear() {
+        itemData.trimToSize(-1)
+    }
 
+    fun delete(key: Int) {
+        itemData.remove(key)
+    }
+
+    fun getItem(postion: Int, notNull: Boolean = false): K? {
+        starBackgroundTask()
+        var data = itemData[postion]
+        val down = postion >= lastPosition
         lastPosition = postion
-//        Log.d(itemData.snapshot().entries.map { "[${it.key},${it.value}]," }.toString())
-//        Log.d("firstOrNull=${itemData.snapshot().keys.firstOrNull() ?: 0}," +
-//                "lastOrNull=${itemData.snapshot().keys.lastOrNull() ?: 0}")
-//        Log.d("down=$down")
+
         if (data == null) {
-            offerToTask(postion, down, false)
+            if (offerToTask(postion, down, false)) {
+                if (notNull) {
+                    work()
+                    data = getItem(postion, notNull)
+                } else {
+                    synchronized(objecz, { objecz.notify() })
+                }
+            }
         } /*else {
             val itemPos = if (down) {
                 section[1]
@@ -78,17 +98,18 @@ class DataLoader<out K, VH : RecyclerView.ViewHolder>(private val adapter: Recyc
         return data
     }
 
-    private fun offerToTask(position: Int, down: Boolean, b: Boolean = false) {
+    private fun offerToTask(position: Int, down: Boolean, b: Boolean = false): Boolean {
         val offer = (0 until task.size).none { position in task[it] until task[it] + SECTION }
         if (offer && b) Log.d("prev load $position")
-        val pos = if (!down) position - SECTION + 1 else position
+        val pos = if (!down) Math.max(position - SECTION + 1, 0) else position
 //        if (task.size == 0 || pos !in task.last until task.last + SECTION) {
         if (offer) {
             Log.d("load offer $pos")
             task.offer(pos)
             if (task.size > 3) task.poll()
-            synchronized(objecz, { objecz.notify() })
+            return true
         }
+        return false
     }
 
     private fun starBackgroundTask() {
